a755714c7891652ec5b2f1cf73282f9bf235dc6f
2017-kocaeli-007 <by.yalcin.batuhan@gmail.com>
2017-12-15 03:59:38 +0200

    Home and ways
    There is a page to be home or way

---
+++ home.php
@@ -0,0 +1,32 @@
+<?php
+require_once("header.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="tr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<title>Arama motoru</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<meta charset="UTF-8">
+
+
+	<h5>Yaz Lab 1 - Proje 3</h5>
+	<h1>Yerli Arama Motoru</h1>
+	<h3>150202065 Batuhan YALCIN</h3>
+	<h3>150202102 Ahmet Tunay OZTURK</h3>
+
+
+	
+
+	<a href="asama1.php"><p>Asama 1</p></a>
+	<a href="asama2.php"><p>Asama 2</p></a>
+	<a href="asama3.php"><p>Asama 3</p></a>
+
+
+</body>
+</html>
+<?php
+require_once("footer.php");
+?>
---
+++ others.php
@@ -0,0 +1,30 @@
+<?php
+require_once("header.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="tr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<title>Arama motoru</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<meta charset="UTF-8">
+
+
+
+	<h1>There is no page to be seen </h1>
+
+
+
+	<h5>There is a <a href="home.php">home</a> you can always go. <br> Or other ways to go <a href="asama1.php">Asama 1</a> <a href="asama2.php">Asama 2</a> <a href="asama3.php">Asama 3</a> </h5>
+
+
+<p>There is ways to go but you need to be brave or crazy to go </p>
+
+
+</body>
+</html>
+<?php
+require_once("footer.php");
+?>
